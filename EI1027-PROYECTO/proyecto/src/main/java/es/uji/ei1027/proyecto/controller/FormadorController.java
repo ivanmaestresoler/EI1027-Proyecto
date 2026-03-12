e0f7459c4390c88f4f305abb1bb85be176772cf8
ivanmaestresoler <ivanmsoler@gmail.com>
2026-03-12 12:47:24 +0100

    Funciones CRUD Formador en Vista

--- EI1027-PROYECTO/proyecto/src/main/java/es/uji/ei1027/proyecto/controller/FormadorController.java
+++ EI1027-PROYECTO/proyecto/src/main/java/es/uji/ei1027/proyecto/controller/FormadorController.java
@@ -1,10 +1,14 @@
 package es.uji.ei1027.proyecto.controller;
 
 import es.uji.ei1027.proyecto.dao.FormadorDao;
+import es.uji.ei1027.proyecto.model.Formador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/formador")
@@ -21,5 +25,35 @@ public class FormadorController {
     public String listFormadors(Model model) {
         model.addAttribute("formadors", formadorDao.getFormadors());
         return "formador/list";
+    }
+
+    @RequestMapping(value="/add") 
+    public String addFormador(Model model) {
+        model.addAttribute("formador", new Formador());
+        return "formador/add";
+    }
+
+    @RequestMapping(value="/add", method=RequestMethod.POST) 
+    public String processAddSubmit(@ModelAttribute("formador") Formador formador) { 
+        formadorDao.addFormador(formador); 
+        return "redirect:/formador/list"; 
+    }
+
+    @RequestMapping(value="/update/{id}", method = RequestMethod.GET)
+    public String editFormador(Model model, @PathVariable int id) {
+        model.addAttribute("formador", formadorDao.getFormador(id));
+        return "formador/update";
+    }
+
+    @RequestMapping(value="/update", method = RequestMethod.POST)
+    public String processUpdateSubmit(@ModelAttribute("formador") Formador formador) {
+        formadorDao.updateFormador(formador);
+        return "redirect:/formador/list";
+    }
+
+    @RequestMapping(value="/delete/{id}")
+    public String processDelete(@PathVariable int id) {
+        formadorDao.deleteFormador(id);
+        return "redirect:/formador/list";
     }
 }
